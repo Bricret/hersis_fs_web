b817eddbfcf6d210cb283f7b330cab0a21252650
Brian Rico <114421216+Bricret@users.noreply.github.com>
2025-03-12 10:36:28 -0600

    add: agregar nueva imagen de portada y eliminar archivos SVG no utilizados

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,26 +1,8 @@
 "use client";
 
 import { LoginForm } from "@/presentation/components/auth/FormLogin";
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
 
 export default function LoginPage() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-
-  const handleLogin = (e: React.FormEvent) => {
-    e.preventDefault();
-    setLoading(true);
-
-    // Simulate login process
-    setTimeout(() => {
-      setLoading(false);
-      router.push("/");
-    }, 1500);
-  };
-
-  const currentYear = new Date().getFullYear();
-
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
